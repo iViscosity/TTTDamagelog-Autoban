7a86e112e873b9f8566a571eb852b9f6e4425d53
Vis <iviscositygaming@gmail.com>
2020-07-07 16:41:52 -0400

    Convert settings to ConVars

--- lua/ulib/modules/server/aban.lua
+++ lua/ulib/modules/server/aban.lua
@@ -1,34 +1,28 @@
 ABAN = {}
-ABAN.Version = 20200414
+ABAN.Version	= 20200707
 
-ABAN.Config = {}
-ABAN.Config.Bantime = 1440 -- The time (in minutes) to ban someone for leaving with slays. Default: 1440 (1 day)
-ABAN.Config.Multiply = true -- Whether or not to multiply this time by how many slays a person has
-ABAN.Config.NotifyAll = true -- Whether or not to let everyone know a person has been automatically banned.
-ABAN.Config.ExemptAdmins = true -- Whether or not to exempt (ignore) admins.
-ABAN.Config.LowestAdminGroup = "operator" -- In your inheritance chain, this is the lowest rank that you'd like to be exempted if ExemptAdmins is true.
-ABAN.Config.MinSlays = 1 -- The minimum amount of slays a player must have to be automatically banned.
-ABAN.Config.Prefix = "[Autoban] " -- Prefix to automated message (i.e. PREFIX .. *player* has been banned")
+local bantime	= CreateConVar("aban_bantime", "1440", {FCVAR_ARCHIVE, FCVAR_NEVER_AS_STRING}, "The time (in minutes) to ban someone for leaving with slays. Default: 1440 (1 day)", 0)
+local multiply	= CreateConVar("aban_multiply", "1", {FCVAR_ARCHIVE, FCVAR_NEVER_AS_STRING}, "Whether or not to multiply bantime by how many slays a person has.")
+local notify	= CreateConVar("aban_notify", "1", {FCVAR_ARCHIVE, FCVAR_NEVER_AS_STRING}, "Whether or not to let everyone know a person has been automatically banned.")
+local exempt	= CreateConVar("aban_exempt_admins", "1", {FCVAR_ARCHIVE, FCVAR_NEVER_AS_STRING}, "Whether or not to exempt (ignore) admins.")
+local lowest	= CreateConVar("aban_lowest_admin_group", "operator", {FCVAR_ARCHIVE, FCVAR_PRINTABLEONLY}, "In your inheritance chain, this is the lowest rank that you'd like to be exempted if aban_exempt_admins is \"1\".")
+local minslays	= CreateConVar("aban_min_slays", "1", {FCVAR_ARCHIVE, FCVAR_NEVER_AS_STRING}, "The minimum amount of slays a player must have to be automatically banned.")
+
+local prefix	= "[Autoban] "
 
 function ABAN.BanPlayer(ply, slays)
 	local reason = "Banned for leaving with %i %s"
 	local notif = "%s was banned %s for leaving with %i %s."
-	local timeStr = ""
-	local numStr = ""
-	local time = ABAN.Config.Bantime
-	local mult = ABAN.Config.Multiply
+	local time = bantime:GetInt()
 
-	if mult then
+	local timeStr, numStr
+
+	if multiply:GetBool() then
 		time = time * slays
 	end
 
-	if time == 0 then
-		timeStr = "permanently"
-	elseif time == 1440 then
-		timeStr = "for 1 day"
-	else
-		timeStr = "for " .. tostring(math.Round((time / 60) / 24)) .. " days"
-	end
+	local secs = time * 60
+	timeStr = ULib.secondsToStringTime(secs) -- easier than doing it myself
 
 	if slays == 1 then
 		numStr = "slay"
@@ -36,15 +30,18 @@ function ABAN.BanPlayer(ply, slays)
 		numStr = "slays"
 	end
 
-	ULib.addBan(ply:SteamID(), time, ABAN.Config.Prefix .. string.format(reason, slays, numStr), ply:Nick())
+	ULib.addBan(ply:SteamID(), time, prefix .. string.format(reason, slays, numStr), ply:Nick())
 
-	if ABAN.Config.NotifyAll then
-		ULib.tsayError(_, ABAN.Config.Prefix .. string.format(notif, ply:Nick(), timeStr, slays, numStr), true)
+	if notify:GetBool() then
+		ULib.tsayError(_, prefix .. string.format(notif, ply:Nick(), timeStr, slays, numStr), true)
 	end
 end
 
 local function CheckForSlays(ply)
-	if not sql.TableExists("damagelog_autoslay") then return end
+	if not sql.TableExists("damagelog_autoslay") then return end -- damagelogs doesn't exist on this server
+	if ULib.bans[ply:SteamID()] then return end -- don't need to do anything if they were already banned
+	if exempt:GetBool() and ply:CheckGroup(lowest:GetString()) then return end
+
 	local data = sql.QueryRow("SELECT slays FROM damagelog_autoslay WHERE ply = '" .. ply:SteamID() .. "' LIMIT 1;")
 	local slays
 
@@ -52,9 +49,9 @@ local function CheckForSlays(ply)
 		slays = tonumber(data.slays)
 	end
 
-	if slays == 0 then return end
-	if ABAN.Config.ExemptAdmins and ply:CheckGroup(ABAN.Config.LowestAdminGroup) then return end
-	if tonumber(slays) < tonumber(ABAN.Config.MinSlays) then return end
+	if not slays
+		or slays == 0
+		or slays < minslays:GetInt() then return end
 
 	ABAN.BanPlayer(ply, slays)
 end
